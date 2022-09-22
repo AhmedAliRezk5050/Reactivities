d8db67497a72d6091d47c0aeae872df948a33944
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-22 15:09:54 +0200

    CQRS + Mediator pattern(the injection is better than RequestServices)

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,16 +7,22 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
+  private readonly IMediator _mediator;
 
-    [HttpGet]
-    public async Task<ActionResult<List<Activity>>> GetActivities()
-    {
-        return await Mediator.Send(new List.Query());
-    }
+  public ActivitiesController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
 
-    [HttpGet("{id:guid}")]
-    public ActionResult GetActivity(Guid id)
-    {
-        return Ok();
-    }
+  [HttpGet]
+  public async Task<ActionResult<List<Activity>>> GetActivities()
+  {
+    return await _mediator.Send(new List.Query());
+  }
+
+  [HttpGet("{id:guid}")]
+  public  ActionResult GetActivity(Guid id)
+  {
+    return Ok();
+  }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,14 +1,10 @@
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public abstract class BaseApiController : ControllerBase
+public class BaseApiController : ControllerBase
 {
-    private IMediator? _mediator;
 
-    protected IMediator Mediator => (_mediator ??= HttpContext.RequestServices.GetService<IMediator>())
-                                    ?? throw new InvalidOperationException();
 }
