63d287995aaf7229ee004cee5df9bf2d589e8408
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-24 13:28:16 +0200

    react folder structure

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
