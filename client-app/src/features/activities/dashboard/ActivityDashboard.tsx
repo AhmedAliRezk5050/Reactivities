15e7a4c612a7fdfe05a6d5926ad3694693cda200
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-29 17:19:40 +0200

    react(add ActivityFilters component)

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,29 +4,25 @@ import ActivityList from "./ActivityList";
 import {useStore} from "../../../app/stores/store";
 import {observer} from "mobx-react-lite";
 import AppSpinner from "../../../app/layout/AppSpinner";
+import ActivityFilters from "./ActivityFilters";
 
 interface Props {
 }
 
 const ActivityDashboard: FC<Props> = () => {
 
-    const {activityStore: {activitiesLoading, error, setError}} = useStore();
+    const {activityStore: {activitiesLoading}} = useStore();
 
     return (
-        <>
-            {error && <Message warning className='message'>
-                <Message.Header>{error.title}</Message.Header>
-                <p>{error.message}</p>
-                <Button icon='close' className='msg-close' onClick={() => setError(null)}/>
-            </Message>}
-            <Grid className='centered'>
+            <Grid>
                 <AppSpinner active={activitiesLoading}/>
-
                 <Grid.Column width='10'>
                     <ActivityList/>
                 </Grid.Column>
+                <Grid.Column width='6'>
+                   <ActivityFilters />
+                </Grid.Column>
             </Grid>
-        </>
 
     );
 };
