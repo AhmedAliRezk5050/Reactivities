f83616d8bca415a3cf1004a462086de7830d58cd
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-19 13:32:48 +0200

    React(Formik(Reducing boilerplate))

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,68 +1,22 @@
-import { Button, Form, Segment } from 'semantic-ui-react';
-import { ChangeEvent, FC, useEffect, useState } from 'react';
+import {ChangeEvent, FC, useEffect, useState} from 'react';
 import Activity from '../../../app/models/activity';
-import { useStore } from '../../../app/stores/store';
-import { observer } from 'mobx-react-lite';
-import { useNavigate, useParams } from 'react-router-dom';
+import {useStore} from '../../../app/stores/store';
+import {observer} from 'mobx-react-lite';
+import {useNavigate, useParams} from 'react-router-dom';
 import AppSpinner from '../../../app/layout/AppSpinner';
 import ErrorsList from '../../errors/ErrorsList';
-import { Formik } from 'formik';
+import {Field, Form, Formik} from 'formik';
 
-interface Props {}
+interface Props {
+}
 
 const ActivityForm: FC<Props> = () => {
-  const { activityStore } = useStore();
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const [localLoading, setLocalLoading] = useState(true);
-  const [formErrors, setFormErrors] = useState<string[]>([]);
-  const [formData, setFormData] = useState<Activity>({
-    id: '',
-    title: '',
-    date: '',
-    description: '',
-    category: '',
-    venue: '',
-    city: '',
-  });
-
-  // const handleChange = (
-  //   e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  // ) => {
-  //   const { name, value } = e.target;
-  //   setFormData({ ...formData, [name]: value });
-  // };
-
-  // const handleSubmit = () => {
-  //   activityStore
-  //     .upsertActivity(formData)
-  //     .catch((e: string[]) => {
-  //       if (Array.isArray(e)) setFormErrors(e);
-  //     })
-  //     .finally(() => {
-  //       if (!activityStore.error) navigate('/activities', { replace: true });
-  //     });
-  // };
-
-  const handleCancel = () => {
-    if (activityStore.error) {
-      activityStore.setError(null);
-    }
-    navigate('/activities', { replace: true });
-  };
-
-  useEffect(() => {
-    if (id) {
-      activityStore
-        .fetchActivity(id)
-        .then(() => {
-          if (activityStore.activity) {
-            setFormData(activityStore.activity);
-          }
-        })
-        .finally(() => setLocalLoading(false));
-    } else {
-      setFormData({
+    const {activityStore} = useStore();
+    const navigate = useNavigate();
+    const {id} = useParams();
+    const [localLoading, setLocalLoading] = useState(true);
+    const [formErrors, setFormErrors] = useState<string[]>([]);
+    const [formData, setFormData] = useState<Activity>({
         id: '',
         title: '',
         date: '',
@@ -70,97 +24,82 @@ const ActivityForm: FC<Props> = () => {
         category: '',
         venue: '',
         city: '',
-      });
-    }
-  }, [id, activityStore]);
+    });
 
-  if (id && (activityStore.activityLoading || localLoading))
-    return <AppSpinner active={activityStore.activityLoading} />;
+    // const handleChange = (
+    //   e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    // ) => {
+    //   const { name, value } = e.target;
+    //   setFormData({ ...formData, [name]: value });
+    // };
 
-  return (
-    <Formik
-      initialValues={formData}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          console.log(values);
-          setSubmitting(false);
-        }, 2000);
-      }}
-    >
-      {({ values, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type='title'
-            name='title'
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.title}
-          />
-          <button type='submit' disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  );
-  // return (
-  //   <Segment clearing>
-  //     <ErrorsList errors={formErrors} />
-  //     <Form onSubmit={handleSubmit}>
-  //       <Form.Input
-  //         placeholder='Title'
-  //         name='title'
-  //         onChange={handleChange}
-  //         value={formData.title}
-  //       />
-  //       <Form.TextArea
-  //         placeholder='Description'
-  //         name='description'
-  //         onChange={handleChange}
-  //         value={formData.description}
-  //       />
-  //       <Form.Input
-  //         placeholder='Category'
-  //         name='category'
-  //         onChange={handleChange}
-  //         value={formData.category}
-  //       />
-  //       <Form.Input
-  //         placeholder='Date'
-  //         type='date'
-  //         name='date'
-  //         onChange={handleChange}
-  //         value={formData.date}
-  //       />
-  //       <Form.Input
-  //         placeholder='City'
-  //         name='city'
-  //         onChange={handleChange}
-  //         value={formData.city}
-  //       />
-  //       <Form.Input
-  //         placeholder='Venue'
-  //         name='venue'
-  //         onChange={handleChange}
-  //         value={formData.venue}
-  //       />
-  //       <Button
-  //         floated='left'
-  //         positive
-  //         type='submit'
-  //         content='Submit'
-  //         loading={activityStore.operationsLoading}
-  //       />
-  //       <Button
-  //         floated='right'
-  //         negative
-  //         type='button'
-  //         content='Cancel'
-  //         onClick={handleCancel}
-  //       />
-  //     </Form>
-  //   </Segment>
-  // );
+    // const handleSubmit = () => {
+    //   activityStore
+    //     .upsertActivity(formData)
+    //     .catch((e: string[]) => {
+    //       if (Array.isArray(e)) setFormErrors(e);
+    //     })
+    //     .finally(() => {
+    //       if (!activityStore.error) navigate('/activities', { replace: true });
+    //     });
+    // };
+
+    const handleCancel = () => {
+        if (activityStore.error) {
+            activityStore.setError(null);
+        }
+        navigate('/activities', {replace: true});
+    };
+
+    useEffect(() => {
+        if (id) {
+            activityStore
+                .fetchActivity(id)
+                .then(() => {
+                    if (activityStore.activity) {
+                        setFormData(activityStore.activity);
+                    }
+                })
+                .finally(() => setLocalLoading(false));
+        } else {
+            setFormData({
+                id: '',
+                title: '',
+                date: '',
+                description: '',
+                category: '',
+                venue: '',
+                city: '',
+            });
+        }
+    }, [id, activityStore]);
+
+    if (id && (activityStore.activityLoading || localLoading))
+        return <AppSpinner active={activityStore.activityLoading}/>;
+
+    return (
+        <Formik
+            initialValues={formData}
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    console.log(values);
+                    setSubmitting(false);
+                }, 2000);
+            }}
+        >
+            {({isSubmitting}) => (
+                <Form>
+                    <Field
+                        type='title'
+                        name='title'
+                    />
+                    <button type='submit' disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    );
 };
 
 export default observer(ActivityForm);
