2907962c409a929ef992154b5db2675d58ac350e
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-21 23:11:24 +0200

    Server(Identity(Get authenticated user(remove unnecessary authorize attribute)))

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -70,7 +70,6 @@ public class AccountController : BaseApiController
         return CreateUserDto(user);
     }
 
-    [Authorize]
     [HttpGet]
     public async Task<ActionResult<UserDto>> GetAuthenticatedUser()
     {
