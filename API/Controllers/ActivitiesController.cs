25c3dfbf48f911a340f9670c6cd077950efb7731
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-22 01:26:01 +0200

    Server(disable authentication)

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -7,44 +7,45 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers;
 
+[AllowAnonymous]
 public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator;
-    private readonly IValidator<Activity> _validator;
+  private readonly IMediator _mediator;
+  private readonly IValidator<Activity> _validator;
 
-    public ActivitiesController(IMediator mediator, IValidator<Activity> validator)
-    {
-        _mediator = mediator;
-        _validator = validator;
-    }
+  public ActivitiesController(IMediator mediator, IValidator<Activity> validator)
+  {
+    _mediator = mediator;
+    _validator = validator;
+  }
 
-    [HttpGet]
-    public async Task<ActionResult> GetActivities()
-    {
-        return HandleResult(await _mediator.Send(new List.Query()));
-    }
+  [HttpGet]
+  public async Task<ActionResult> GetActivities()
+  {
+    return HandleResult(await _mediator.Send(new List.Query()));
+  }
 
-    [HttpGet("{id:guid}")]
-    public async Task<ActionResult> GetActivity(Guid id)
-    {
-        return HandleResult(await _mediator.Send(new Details.Query { Id = id }));
-    }
+  [HttpGet("{id:guid}")]
+  public async Task<ActionResult> GetActivity(Guid id)
+  {
+    return HandleResult(await _mediator.Send(new Details.Query { Id = id }));
+  }
 
-    [HttpPost]
-    public async Task<ActionResult> CreateActivity(Activity activity)
-    {
-        return HandleResult(await _mediator.Send(new Create.Command { Activity = activity }));
-    }
+  [HttpPost]
+  public async Task<ActionResult> CreateActivity(Activity activity)
+  {
+    return HandleResult(await _mediator.Send(new Create.Command { Activity = activity }));
+  }
 
-    [HttpPut("{id:guid}")]
-    public async Task<ActionResult> EditActivity(Guid id, Activity activity)
-    {
-        return HandleResult(await _mediator.Send(new Edit.Command { Activity = activity, Id = id}));
-    }
+  [HttpPut("{id:guid}")]
+  public async Task<ActionResult> EditActivity(Guid id, Activity activity)
+  {
+    return HandleResult(await _mediator.Send(new Edit.Command { Activity = activity, Id = id }));
+  }
 
-    [HttpDelete("{id:guid}")]
-    public async Task<ActionResult> DeleteActivity(Guid id)
-    {
-        return HandleResult(await _mediator.Send(new Delete.Command() { Id = id }));
-    }
+  [HttpDelete("{id:guid}")]
+  public async Task<ActionResult> DeleteActivity(Guid id)
+  {
+    return HandleResult(await _mediator.Send(new Delete.Command() { Id = id }));
+  }
 }
