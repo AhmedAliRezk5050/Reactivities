7e030239afd180c591461f5bc960df75f1f24b8c
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-27 01:47:58 +0200

    Client(Edits) edit routing paths

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,26 @@
-import RootLayout from "./RootLayout/RootLayout";
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../../features/home/Home";
-import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
-import NotFound from "../../features/errors/not-found/NotFound";
-import AppRouter from "../../routing/AppRouter";
+import RootLayout from './RootLayout/RootLayout';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from '../../features/home/Home';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import NotFound from '../../features/errors/not-found/NotFound';
+import AppRouter from '../../routing/AppRouter';
 
 const App = () => {
   return (
     <AppRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<RootLayout />}>
-          <Route path="activities" element={<ActivityDashboard />} />
-          <Route path="activities/create" element={<ActivityForm />} />
-          <Route path="activities/not-found" element={<NotFound />} />
-          <Route path="activities/:id" element={<ActivityDetails />} />
-          <Route path="activities/:id/edit" element={<ActivityForm />} />
-          <Route path="not-found" element={<NotFound />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/*' element={<RootLayout />}>
+          <Route path='activities' element={<ActivityDashboard />} />
+          <Route path='activities/create' element={<ActivityForm />} />
+          <Route path='activities/not-found' element={<NotFound />} />
+          <Route path='activities/:id' element={<ActivityDetails />} />
+          <Route path='activities/:id/edit' element={<ActivityForm />} />
+          <Route path='not-found' element={<NotFound />} />
         </Route>
-        <Route path="*" element={<Navigate to="not-found" replace />} />
+        <Route path='*' element={<Navigate to='not-found' replace />} />
       </Routes>
     </AppRouter>
   );
--- client-app/src/app/layout/NavBar/NavBar.tsx
+++ client-app/src/app/layout/NavBar/NavBar.tsx
@@ -1,9 +1,9 @@
-import { Button, Container, Menu, Image, Dropdown } from "semantic-ui-react";
-import "./NavBar.css";
-import { FC } from "react";
-import { observer } from "mobx-react-lite";
-import { Link, NavLink } from "react-router-dom";
-import { useStore } from "../../stores/store";
+import { Button, Container, Menu, Image, Dropdown } from 'semantic-ui-react';
+import './NavBar.css';
+import { FC } from 'react';
+import { observer } from 'mobx-react-lite';
+import { Link, NavLink } from 'react-router-dom';
+import { useStore } from '../../stores/store';
 
 interface Props {}
 
@@ -13,32 +13,32 @@ const NavBar: FC<Props> = () => {
   } = useStore();
 
   return (
-    <Menu inverted fixed="top" className="NavBar">
+    <Menu inverted fixed='top' className='NavBar'>
       <Container>
-        <Menu.Item header as={NavLink} to="/home">
-          <img src="/assets/logo.png" alt="logo" className="logo" />
+        <Menu.Item header as={NavLink} to='/'>
+          <img src='/assets/logo.png' alt='logo' className='logo' />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" as={NavLink} to="/activities" end />
-        <Menu.Item as={NavLink} to="/activities/create" end>
-          <Button content="Create Activity" />
+        <Menu.Item name='Activities' as={NavLink} to='/activities' end />
+        <Menu.Item as={NavLink} to='/activities/create' end>
+          <Button content='Create Activity' />
         </Menu.Item>
-        <Menu.Item position="right">
+        <Menu.Item position='right'>
           <Image
-            src={user?.image || "/assets/user.png"}
+            src={user?.image || '/assets/user.png'}
             avatar
-            spaced="right"
+            spaced='right'
           />
 
-          <Dropdown text={user?.displayName} pointing="top left">
+          <Dropdown text={user?.displayName} pointing='top left'>
             <Dropdown.Menu>
               <Dropdown.Item as={Link} to={`/profile/${user?.userName}`}>
                 My Profile
               </Dropdown.Item>
               <Dropdown.Item
                 onClick={() => logout()}
-                icon="power"
-                text="Logout"
+                icon='power'
+                text='Logout'
               />
             </Dropdown.Menu>
           </Dropdown>
