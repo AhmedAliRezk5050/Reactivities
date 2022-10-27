3f09141ea0f5b771cfb2595d1cf7dd3c6eb4891b
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-10-27 21:02:01 +0200

    Client(image upload) Crop Photo(add react-cropper) Create(CropperPhotoWidget)

--- client-app/src/common/photo-upload/PhotoUploadWidget.tsx
+++ client-app/src/common/photo-upload/PhotoUploadWidget.tsx
@@ -1,9 +1,25 @@
-import { useState } from 'react';
-import { Grid, GridColumn, Header, Image } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Button, Grid, GridColumn, Header } from 'semantic-ui-react';
+import CropperPhotoWidget from './CropperPhotoWidget';
 import DropZonePhotoWidget from './DropZonePhotoWidget';
 
 const PhotoUploadWidget = () => {
   const [files, setFiles] = useState<any>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+
+  const onCrop = () => {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob((blob) => console.log(blob));
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      // clean from memory after component destructuring
+      files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+    };
+  }, [files]);
+
   return (
     <Grid style={{ textAlign: 'center' }}>
       <Grid.Column width={1} />
@@ -20,13 +36,34 @@ const PhotoUploadWidget = () => {
           <p>Step 2</p>
           <p>Resize Image</p>
         </Header>
-        {files.length > 0 && <Image src={files[0].preview} />}
+        {files.length > 0 && (
+          <CropperPhotoWidget
+            setCropper={setCropper}
+            imagePreview={files[0].preview}
+          />
+        )}
       </GridColumn>
       <Grid.Column width={1} />
       <GridColumn width={4}>
         <Header color='grey'>
           <p>Step 3</p>
           <p>Preview & Upload</p>
+          {files.length > 0 && (
+            <>
+              <div
+                className='img-preview'
+                style={{
+                  minHeight: 200,
+                  overflow: 'hidden',
+                  marginBottom: '10px',
+                }}
+              />
+              <Button.Group>
+                <Button onClick={onCrop} positive icon='check' />
+                <Button onClick={() => setFiles([])} icon='close' />
+              </Button.Group>
+            </>
+          )}
         </Header>
       </GridColumn>
     </Grid>
