c27ed654be034950dafa4447706e33c1f9e7b1bd
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-03 12:32:28 +0200

    Server(SignalR) Authenticating to SignalR

--- API/Extensions.cs
+++ API/Extensions.cs
@@ -13,59 +13,74 @@ namespace API;
 
 public static class Extensions
 {
-    public static void ConfigureCors(this IServiceCollection services)
+  public static void ConfigureCors(this IServiceCollection services)
+  {
+    services.AddCors(o =>
     {
-        services.AddCors(o =>
-        {
-            o.AddPolicy("CorsPolicy",
-                policyBuilder =>
-                    policyBuilder
-                        // .AllowAnyOrigin()
-                        .WithOrigins("http://localhost:3000")
-                        .AllowAnyMethod()
-                        .AllowAnyHeader());
-        });
-    }
+      o.AddPolicy("CorsPolicy",
+              policyBuilder =>
+                  policyBuilder
+                      // .AllowAnyOrigin()
+                      .WithOrigins("http://localhost:3000")
+                      .AllowAnyMethod()
+                      .AllowAnyHeader());
+    });
+  }
 
-    public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<DataContext>(options =>
+  public static void ConfigureDbContext(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddDbContext<DataContext>(options =>
+        {
+          options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        }
+    );
+  }
+
+  public static void ConfigureIdentity(this IServiceCollection services, IConfiguration config)
+  {
+    services.AddIdentityCore<AppUser>(options => { options.Password.RequireNonAlphanumeric = false; })
+        .AddEntityFrameworkStores<DataContext>()
+        .AddSignInManager<SignInManager<AppUser>>();
+
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(o =>
+        {
+          o.TokenValidationParameters = new TokenValidationParameters
+          {
+            // validate token against our provided secret key("super secret key")
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+            ValidateIssuer = false,
+            ValidateAudience = false
+
+          };
+
+          o.Events = new JwtBearerEvents
+          {
+            OnMessageReceived = context =>
             {
-                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+              var accessToken = context.Request.Query["access_token"];
+              var path = context.HttpContext.Request.Path;
+              if (!string.IsNullOrEmpty(accessToken) &&
+                  (path.StartsWithSegments("/chat")))
+              {
+                context.Token = accessToken;
+              }
+              return Task.CompletedTask;
             }
-        );
-    }
-
-    public static void ConfigureIdentity(this IServiceCollection services, IConfiguration config)
-    {
-        services.AddIdentityCore<AppUser>(options => { options.Password.RequireNonAlphanumeric = false; })
-            .AddEntityFrameworkStores<DataContext>()
-            .AddSignInManager<SignInManager<AppUser>>();
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
-        
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(o =>
-            {
-                o.TokenValidationParameters = new TokenValidationParameters
-                {
-                    // validate token against our provided secret key("super secret key")
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = key,
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                        
-                };
-            });
-
-        services.AddAuthorization(options =>
-        {
-            options.AddPolicy("IsActivityHost", policy =>
-            {
-                policy.Requirements.Add(new HostRequirement());
-            });
+          };
         });
-        services.AddTransient<IAuthorizationHandler, HostRequirementHandler>();
-        services.AddScoped<AuthService>();
-    }
+
+    services.AddAuthorization(options =>
+    {
+      options.AddPolicy("IsActivityHost", policy =>
+          {
+          policy.Requirements.Add(new HostRequirement());
+        });
+    });
+    services.AddTransient<IAuthorizationHandler, HostRequirementHandler>();
+    services.AddScoped<AuthService>();
+  }
 }
