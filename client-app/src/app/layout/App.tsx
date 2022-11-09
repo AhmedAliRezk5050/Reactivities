994ea48920df7b186f90e916c6d1d51ac76e8f1a
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-09 02:59:37 +0200

    Client(Add ScrollToTop component) Add scroll to top functionality on navigation

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,39 @@
-import RootLayout from './RootLayout/RootLayout';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Home from '../../features/home/Home';
-import ActivityForm from '../../features/activities/form/ActivityForm';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import ActivityDetails from '../../features/activities/details/ActivityDetails';
-import NotFound from '../../features/errors/not-found/NotFound';
-import AppRouter from '../../routing/AppRouter';
-import ProfilePage from '../../features/profiles/ProfilePage';
+import RootLayout from "./RootLayout/RootLayout";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../../features/home/Home";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "../../features/errors/not-found/NotFound";
+import AppRouter from "../../routing/AppRouter";
+import ProfilePage from "../../features/profiles/ProfilePage";
+import AppModal from "./modals/AppModal";
+import { ToastContainer } from "react-toastify";
+import React from "react";
+import ScrollToTop from "./ScrollToTop";
 
 const App = () => {
   return (
-    <AppRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/*' element={<RootLayout />}>
-          <Route path='activities' element={<ActivityDashboard />} />
-          <Route path='activities/create' element={<ActivityForm />} />
-          <Route path='activities/not-found' element={<NotFound />} />
-          <Route path='activities/:id' element={<ActivityDetails />} />
-          <Route path='activities/:id/edit' element={<ActivityForm />} />
-          <Route path='profiles/:username' element={<ProfilePage />} />
-          <Route path='not-found' element={<NotFound />} />
-          <Route path='*' element={<Navigate to='not-found' replace />} />
-        </Route>
-      </Routes>
-    </AppRouter>
+    <>
+      <AppModal />
+      <ToastContainer position="bottom-right" hideProgressBar />
+      <AppRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<RootLayout />}>
+            <Route path="activities" element={<ActivityDashboard />} />
+            <Route path="activities/create" element={<ActivityForm />} />
+            <Route path="activities/not-found" element={<NotFound />} />
+            <Route path="activities/:id" element={<ActivityDetails />} />
+            <Route path="activities/:id/edit" element={<ActivityForm />} />
+            <Route path="profiles/:username" element={<ProfilePage />} />
+            <Route path="not-found" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="not-found" replace />} />
+          </Route>
+        </Routes>
+      </AppRouter>
+    </>
   );
 };
 
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import 'react-calendar/dist/Calendar.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import { store, StoreContext } from './app/stores/store';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'react-datepicker/dist/react-datepicker.css';
-import AppModal from './app/layout/modals/AppModal';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "react-calendar/dist/Calendar.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import { store, StoreContext } from "./app/stores/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-datepicker/dist/react-datepicker.css";
+import AppModal from "./app/layout/modals/AppModal";
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
     <App />
-    <AppModal />
-    <ToastContainer position='bottom-right' hideProgressBar />
   </StoreContext.Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
