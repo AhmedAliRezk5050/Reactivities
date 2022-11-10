97b55572683057fe3b8e2ab5d6185d83c15252c9
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-11-10 03:28:13 +0200

    Facebook Login Client(use https instead of http) Add init and login methods

--- client-app/src/app/stores/authStore.ts
+++ client-app/src/app/stores/authStore.ts
@@ -1,16 +1,16 @@
-import { LoginData, RegisterData } from './../models/user';
-import { makeAutoObservable, reaction } from 'mobx';
-import { User } from '../models/user';
-import { authApi } from '../api/agent';
-import { appBrowserHistory } from '../../routing/AppRouter';
-import { store } from './store';
+import { LoginData, RegisterData } from "./../models/user";
+import { makeAutoObservable, reaction } from "mobx";
+import { User } from "../models/user";
+import { authApi } from "../api/agent";
+import { appBrowserHistory } from "../../routing/AppRouter";
+import { store } from "./store";
 
 export default class AuthStore {
   user: User | null = null;
 
   constructor() {
     makeAutoObservable(this);
-    const userFromLocalStorage = localStorage.getItem('user');
+    const userFromLocalStorage = localStorage.getItem("user");
     if (userFromLocalStorage) {
       this.setUser(JSON.parse(userFromLocalStorage));
     }
@@ -19,16 +19,16 @@ export default class AuthStore {
       () => this.user,
       (user) => {
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          localStorage.setItem('token', user.token);
-          appBrowserHistory.replace('/activities');
+          localStorage.setItem("user", JSON.stringify(user));
+          localStorage.setItem("token", user.token);
+          appBrowserHistory.replace("/activities");
         } else {
-          localStorage.removeItem('user');
-          localStorage.removeItem('token');
-          appBrowserHistory.replace('/');
+          localStorage.removeItem("user");
+          localStorage.removeItem("token");
+          appBrowserHistory.replace("/");
         }
         store.modalStore.closeModal();
-      },
+      }
     );
   }
 
@@ -57,6 +57,15 @@ export default class AuthStore {
     this.setUser(null);
   };
 
+  facebookLogin = () => {
+    FB.login(
+      (response) => {
+        console.log(response);
+      },
+      { scope: "public_profile,email" }
+    );
+  };
+
   get authenticated() {
     return !!this.user;
   }
@@ -64,14 +73,14 @@ export default class AuthStore {
   setImage = (image: string) => {
     if (this.user) {
       this.user.image = image;
-      localStorage.setItem('user', JSON.stringify(this.user));
+      localStorage.setItem("user", JSON.stringify(this.user));
     }
   };
 
   setDisplayName = (displayName: string) => {
     if (this.user) {
       this.user.displayName = displayName;
-      localStorage.setItem('user', JSON.stringify(this.user));
+      localStorage.setItem("user", JSON.stringify(this.user));
     }
   };
 }
--- client-app/src/features/home/Home.tsx
+++ client-app/src/features/home/Home.tsx
@@ -1,46 +1,62 @@
-import { observer } from 'mobx-react-lite';
-import { Link } from 'react-router-dom';
-import { Button, Container, Header, Image, Segment } from 'semantic-ui-react';
-import { useStore } from '../../app/stores/store';
-import RegisterForm from '../users/RegisterForm';
-import LoginForm from '../users/LoginForm';
+import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Container,
+  Divider,
+  Header,
+  Image,
+  Segment,
+} from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import RegisterForm from "../users/RegisterForm";
+import LoginForm from "../users/LoginForm";
 
 const Home = () => {
   const { authStore, modalStore } = useStore();
 
   return (
-    <Segment inverted textAlign='center' vertical className='masthead'>
+    <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
-        <Header as='h1' inverted>
+        <Header as="h1" inverted>
           <Image
-            size='massive'
-            src='/assets/logo.png'
-            alt='logo'
-            style={{ marginBottom: '.75rem' }}
+            size="massive"
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginBottom: ".75rem" }}
           />
           Reactivities
         </Header>
-        <Header as='h2' inverted content='Welcome to Reactivities' />
+        <Header as="h2" inverted content="Welcome to Reactivities" />
         {authStore.authenticated ? (
-          <Button as={Link} to='/activities' size='huge' inverted>
+          <Button as={Link} to="/activities" size="huge" inverted>
             Take me to the Activities
           </Button>
         ) : (
           <>
             <Button
               onClick={() => modalStore.openModal(<LoginForm />)}
-              size='huge'
+              size="huge"
               inverted
             >
               Login
             </Button>
             <Button
               onClick={() => modalStore.openModal(<RegisterForm />)}
-              size='huge'
+              size="huge"
               inverted
             >
               Register
             </Button>
+            <Divider horizontal inverted>
+              Or
+            </Divider>
+            <Button
+              size="huge"
+              inverted
+              content="Login with Facebook"
+              onClick={authStore.facebookLogin}
+            ></Button>
           </>
         )}
       </Container>
